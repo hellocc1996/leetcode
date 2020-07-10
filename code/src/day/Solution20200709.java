7d7ee4e96f747fdcd4997c666b64d3a9b1ce5fee
cuipingping <cuipingping@nicomama.com>
2020-07-10 17:26:40 +0800

    提交

---
+++ code/src/day/Solution20200709.java
@@ -0,0 +1,53 @@
+package day;
+
+/**
+* @author : cuipingping
+* create at:  2020/7/9
+* 
+* 哦，不！你不小心把一个长篇文章中的空格、标点都删掉了，并且大写也弄成了小写。
+* 像句子"I reset the computer. It still didn’t boot!"已经变成了"iresetthecomputeritstilldidntboot"。
+* 在处理标点符号和大小写之前，你得先把它断成词语。
+* 
+* 当然了，你有一本厚厚的词典dictionary，不过，有些词没在词典里。
+* 假设文章用sentence表示，设计一个算法，把文章断开，要求未识别的字符最少，返回未识别的字符数。
+* 
+* 注意：本题相对原题稍作改动，只需返回未识别的字符数
+ * 
+ * 示例：
+ * 输入：
+ * dictionary = ["looked","just","like","her","brother"]
+ * sentence = "jesslookedjustliketimherbrother"
+ * 输出： 7
+ * 解释： 断句后为"jess looked just like tim her brother"，共7个未识别字符。
+ * 提示：
+ * 0 <= len(sentence) <= 1000
+ * dictionary中总字符数不超过 150000。
+ * 你可以认为dictionary和sentence中只包含小写字母。
+ * 
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/re-space-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution20200709 {
+    public static void main(String[] args) {
+
+    }
+
+    private static int respace(String[] dictionary, String sentence) {
+//        //双指针遍历
+//        int head=0,p=0,length=0;
+//
+//        while (head<sentence.length()){
+//
+//            if(sentence.charAt(p))
+//        }
+
+        return 0;
+
+    }
+}
